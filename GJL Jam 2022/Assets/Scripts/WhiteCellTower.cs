5df12f0705aa621c82bc64a2a9cb3cb6947338cd
Andrew Letailleur <onetwo1up@hotmail.co.uk>
2022-03-15 19:54:46 +0000

    Code adjustments and futureproofed debug
    Probably not much, and maybe not pretty on the '2D on 3D' cheat lens. But aim wise, it at least ensures the turret rotates to nearest enemy, without constantly calling for nearest enemy all the time.
    
    Might be unrealistic, but less likely to be a 'RAM bottleneck', under avoided "foreach" call per EVERY update lens.

--- GJL Jam 2022/Assets/Scripts/WhiteCellTower.cs
+++ GJL Jam 2022/Assets/Scripts/WhiteCellTower.cs
@@ -17,6 +17,7 @@ public class WhiteCellTower : MonoBehaviour
     /// As said, it also has a 'timed' limit on how long it should last in-game.
     /// By default, it's 5 seconds. But maybe, that time limit could be tripled/quadupled?
     /// </summary>
+
     /* References: Time.deltaTime, unity documents wise; under "per last frame" second wise
      * https://docs.unity3d.com/ScriptReference/Time-deltaTime.html
      * CreatePrimative, "generate game objects" out of thin air, for colliders/etc
@@ -29,27 +30,29 @@ public class WhiteCellTower : MonoBehaviour
     #endregion
 
     //publlic variables, to be declared on top. Easy editing wise
-    public float MaxTimer = 5.0f;//seconds. Consider triple/quadupling that timer, if required?
-    public float BPS = 1.0f;//for "one bullet per second", tempo wise
-    private float _bps_timer;//better for bullet count, than = MaxTimer.
-    //Only enable "_timer" if not doing a 'delayed' Destroy, optimization wise.
+        //lifespan timer variables
+    public float LifespanTimer = 5.0f;//seconds of life. Consider triple/quadupling that timer, if required?
+        //Only enable "_timer" if not doing a 'delayed' Destroy, optimization wise.
+
+        //bullet/projectile variables
+    public float BulletsPerSecond = 1.0f;//for "one bullet per second", tempo wise
+    private float _bps_timer;//timer to set, dependant on the public "Bullets Per Second" value
     public float BulletVelocity = 15f;//tinker/taylor, until it is set properly
 
-    //public for now, need to figure out how to auto grab
-    private Transform _firePoint;//where to fire pellet from, instead of "center mass".
+    //public for now, debug wise. But, need to figure out how to auto grab the 'key fire point/transform' of gun.
+    public Transform _firePoint;//where to fire pellet from, instead of "center mass".
 
-    //current target/AOE, depending on 'distance' to nearest enemy/boss?
+    //current target/AOE, depending on 'distance' to nearest enemy/boss per 'tick' check?
     private Transform _curTarget;//this should shift after a check, before firing at position wise.
 
     //to grab/serialize as an easy reference later, GameObject projectile wise
     private GameObject _bullet;//equals a "tiny sphere", to fire/fling at enemy. "Cell" wise...
 
-
     // Start is called before the first frame update
     void Start()
     {
             //first, initiate/setup timers
-        Destroy(gameObject, MaxTimer); //destroy on start/enable/spawn, 
+        Destroy(gameObject, LifespanTimer); //destroy on start/enable/spawn, 
         _bps_timer = 1.0f;//to represent 'a' second
         //_timer = MaxTimer; //only enable this if NOT, Destroy this after a public delay/etc.
 
@@ -76,23 +79,32 @@ public class WhiteCellTower : MonoBehaviour
     private void Update()
     {
         //step A: Scan/assign target, to nearest enemy.
-        //... May, need to do a list check, iterative wise. Which is a drain.
-        TargetEnemy();// foreach inefficient. Could be called at occasionally?
+            //... Leave for 'firing at enemy', to call. Otherwise, keep aim at 'previous target'.
+        //If previous target still active/alive, rotate to that previous 'target', aim wise
+        if (_curTarget != null) //IE: IF there is a Targeted Enemy, adjust scope/aim.
+        { //Consider making it "2.5D" Friendly, to bar 'rotating' turret glitch from target?
+            Vector3 _sameAim = _curTarget.position;//debug 'fix' code, under being mindful to avoid a turret visual bug.
+            _sameAim.y = gameObject.transform.position.y;//to keep the 'aim' of the singular turret on the same viewpoint.
+            gameObject.transform.LookAt(_sameAim); //would be LookAt(_curTarget), IF the 'barrel/head' can rotate without issue
+        }//added lines take into account, the 'turret glitch' if the entire model rotates 'upwards/downwards', visual wise.
 
-        //end, in rotating TO, nearest enemy, "aim/cursor" wise.
-        gameObject.transform.LookAt(_curTarget);
-        //not taking into account, a 'turret glitch' if it goes up/down/sideways, rotation wise.
-
-        //step B: After X seconds? (Timer wise?), fire bullet.
-        if (_bps_timer <= 0.0f) { FireCell(); _bps_timer = 1.0f; }//...after, a delay?
-        else { _bps_timer -= (Time.deltaTime * BPS); }//deduct by firing rate, yo
+        //step B: After X seconds? (Timer wise?), fire bullet. And do an enemy count/check, from those with the "Enemy" Tag/string.
+        if (_bps_timer <= 0.0f) 
+        {//only call TargetEnemy when required. NOT constantly, to 'save' on RAM/memory usage?
+            TargetEnemy();// foreach inefficient. Could be called at occasionally?
+            FireCell(); _bps_timer = 1.0f; 
+        }//...after, a delay?
+        else { _bps_timer -= (Time.deltaTime * BulletsPerSecond); }//deduct by firing rate, yo
 
         //Step C: Countdown/deprecate this Game Object, if enabled here past bullet fire code
+            //not needed, under the lens of it was started at object start.
+            //that said, IF mindful on pause menu. Consider implementing reference here, to make spawn length 'pause safe'.
         /*        _timer -= Time.deltaTime //timercode. Only enable if there's no timed Destroy at Start.
         //        if (_timer <= 0.0f) Destroy(gameObject);//alternate delete, if 
         *////endif
     }
 
+    //counts the amount of enemies with the "Enemy" tag (not boss), iteratively setting aim to the nearest enemy to the turret.
     private void TargetEnemy()
     {//ref: https://docs.unity3d.com/ScriptReference/GameObject.FindGameObjectsWithTag.html
         _curTarget = null;
@@ -118,17 +130,19 @@ public class WhiteCellTower : MonoBehaviour
         }//end foreach loop
     }//end TargetEnemy
 
-
+    //fire a projectile, from the "White Cell tower", defense bullet wise.
     private void FireCell() {
         //hack rotationary, 'flip back' edition
         Quaternion _fireRotate = _firePoint.transform.rotation;
-        _fireRotate.z -= 90f;//jnc, hack wise. Just so that red arrow points forward
-
+        _fireRotate.z -= 90f;//aim adjustment from 'end barrel' stroke, hack wise.
+            //Just so that red arrow points forward. Fix/[REDACT] this, if model or aim point's fixed.
+        //consider an added transform from the 'aim' here, if projectile test bugs upon spawning.
         GameObject go = Instantiate(_bullet, _firePoint.transform.position, _fireRotate);
         Rigidbody rb = go.GetComponent<Rigidbody>();
         //depending on scale, times it by a factor of 1/10/100?
         rb.AddForce(transform.right * BulletVelocity);
         //rb.AddTorque, if wanting cell to 'spin' at random?
+//        Debug.LogWarning("Object is spawned at: " + go.transform.position + ", Time Spawned = " + Time.fixedTime);
     }
 
 
