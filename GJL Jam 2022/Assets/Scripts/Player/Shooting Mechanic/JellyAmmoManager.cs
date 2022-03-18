575de55526691f1d6d97d37b7ba8663d40dc1f2d
Protagonist <49970830+PlotArmorStudios@users.noreply.github.com>
2022-03-18 10:45:25 -0400

    Ui for damage jelly responds only to using damage jelly

--- GJL Jam 2022/Assets/Scripts/Player/Shooting Mechanic/JellyAmmoManager.cs
+++ GJL Jam 2022/Assets/Scripts/Player/Shooting Mechanic/JellyAmmoManager.cs
@@ -1,4 +1,20 @@
+using UnityEngine;
+
 public class JellyAmmoManager : AmmoManager
 {
+    private JellyShotToggler _jellyShotToggler;
+
+    protected override void Start()
+    {
+        base.Start();
+        _jellyShotToggler = GetComponent<JellyShotToggler>();
+    }
     
+    public override void SubtractAmmo()
+    {
+        if (_jellyShotToggler.JellyToShoot == JellyToShoot.DamageJelly)
+        {
+            base.SubtractAmmo();
+        }
+    }
 }
