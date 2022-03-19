c86571387d40f79ddae5474b8713519712e0d62c
Protagonist <49970830+PlotArmorStudios@users.noreply.github.com>
2022-03-19 14:29:06 -0400

    Added menu sounds

---
+++ GJL Jam 2022/Assets/Scripts/UI/PressSound.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressSound : MonoBehaviour
+{
+    public void Press()
+    {
+        //Sound event goes here
+    }
+}
