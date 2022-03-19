81ace859d3a411c7ec1768720233f9d63fb16c32
Protagonist <49970830+PlotArmorStudios@users.noreply.github.com>
2022-03-18 22:32:39 -0400

    Attempting model transport

--- GJL Jam 2022/Assets/Scripts/Player/Shooting Mechanic/JellyShotAimer.cs
+++ GJL Jam 2022/Assets/Scripts/Player/Shooting Mechanic/JellyShotAimer.cs
@@ -5,11 +5,16 @@ public class JellyShotAimer : MonoBehaviour
 {
     [SerializeField] private JellyShotToggler jellyShotToggler;
     [SerializeField] private float _force;
-    [SerializeField] private Camera _cam;
     [SerializeField] private float _defaultRayRange = 1000;
 
     private Vector3 _destination;
     private Vector3 _direction;
+    private Camera _cam;
+
+    private void Start()
+    {
+        _cam = FindObjectOfType<Camera>();
+    }
 
     private void Update()
     {
