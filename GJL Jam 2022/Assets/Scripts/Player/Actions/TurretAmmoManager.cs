bbd7d371bfc368da1ce2657f1f1ad06d11fb6a72
Protagonist <49970830+PlotArmorStudios@users.noreply.github.com>
2022-03-20 17:19:19 -0400

    Revert "Merge branch 'main' into James-2"
    This reverts commit 7c086eb626c97017a55265e3d8f007b2aff093a0, reversing
    changes made to d8b6511231e7c9ea958a4b817c6a04a6822a12c1.

--- GJL Jam 2022/Assets/Scripts/Player/Actions/TurretAmmoManager.cs
+++ GJL Jam 2022/Assets/Scripts/Player/Actions/TurretAmmoManager.cs
@@ -6,12 +6,4 @@ using UnityEngine;
 //Intentionally left blank to house Turret ammo manager specific variables
 public class TurretAmmoManager : AmmoManager
 {
-    public override void AddAmmo()
-    {
-        base.AddAmmo();
-        // reload event goes here
-        Debug.Log("Play turret reload sound");
-
-        AkSoundEngine.PostEvent("Play_UI_Turret_Ready", gameObject);
-    }
 }
