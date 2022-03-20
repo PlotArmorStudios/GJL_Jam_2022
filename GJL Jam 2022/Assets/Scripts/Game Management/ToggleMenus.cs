4c5fb9b4fd5a40bf0c03c5772995fdf0bbccef7a
Protagonist <49970830+PlotArmorStudios@users.noreply.github.com>
2022-03-20 10:42:56 -0400

    Added pause menu

---
+++ GJL Jam 2022/Assets/Scripts/Game Management/ToggleMenus.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleMenus : MonoBehaviour
+{
+    [SerializeField] private GameObject _hudUI;
+    [SerializeField] private GameObject _pauseMenu;
+
+    private void OnEnable()
+    {
+        GameManager.OnGamePause += ToggleMenuOn;
+        GameManager.OnGameUnpause += ToggleMenuOff;
+    }
+    private void OnDisable()
+    {
+        GameManager.OnGamePause -= ToggleMenuOn;
+        GameManager.OnGameUnpause -= ToggleMenuOff;
+    }
+
+    private void ToggleMenuOff()
+    {
+        _pauseMenu.gameObject.SetActive(false);
+        _hudUI.gameObject.SetActive(true);
+    }
+
+    private void ToggleMenuOn()
+    {
+        _pauseMenu.gameObject.SetActive(true);
+        _hudUI.gameObject.SetActive(false);
+    }
+}
