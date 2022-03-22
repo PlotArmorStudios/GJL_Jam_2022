e81e7f8f074575b18e6952007219f83244de9ba2
Protagonist <49970830+PlotArmorStudios@users.noreply.github.com>
2022-03-22 02:22:55 -0400

    Ammo toggle ui working

--- GJL Jam 2022/Assets/Scripts/Player/Shooting Mechanic/Shooting UI/UIJellyToggler.cs
+++ GJL Jam 2022/Assets/Scripts/Player/Shooting Mechanic/Shooting UI/UIJellyToggler.cs
@@ -6,10 +6,14 @@ public class UIJellyToggler : MonoBehaviour
     [SerializeField] private Image _freezingJellyUI;
     [SerializeField] private Image[] _damageJellyUI;
 
+    private DamageJellyAmmoManager _damageJellyAmmoManager;
+    
     private void OnEnable()
     {
         JellyShotToggler.OnToggleDamageJelly += ToggleDamageJellyUI;
         JellyShotToggler.OnToggleFreezingJelly += ToggleFreezingJellyUI;
+        
+        _damageJellyAmmoManager = GetComponent<DamageJellyAmmoManager>();
     }
 
     private void OnDisable()
@@ -31,10 +35,10 @@ public class UIJellyToggler : MonoBehaviour
     private void ToggleDamageJellyUI()
     {
         _freezingJellyUI.color = new Color(1, 1, 1, .3f);
-        
-        foreach (var damageJellyUI in _damageJellyUI)
+
+        for (int i = 0; i < _damageJellyAmmoManager.CurrentAmmo; i++)
         {
-            damageJellyUI.color = new Color(1, 1, 1, 1f);
+            _damageJellyUI[i].color = new Color(1, 1, 1, 1f);
         }
     }
 }
