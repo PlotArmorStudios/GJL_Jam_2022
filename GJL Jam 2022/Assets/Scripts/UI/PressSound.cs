79ddcef4b601eded7aca0fc838fd7afb67a7873f
Protagonist <49970830+PlotArmorStudios@users.noreply.github.com>
2022-03-22 14:47:39 -0400

    Improved cutscene scaling and fixed press sound

--- GJL Jam 2022/Assets/Scripts/UI/PressSound.cs
+++ GJL Jam 2022/Assets/Scripts/UI/PressSound.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class PressSound : MonoBehaviour
 {
-    [SerializeField] private AudioClip _audioClipName;
-    [SerializeField] private AudioSource _audioSourceName;
     public void Press()
     {
-        _audioSourceName.PlayOneShot(_audioClipName);
         AkSoundEngine.PostEvent("Play_UI_gamestart_or_reset", gameObject);
     }
 }
