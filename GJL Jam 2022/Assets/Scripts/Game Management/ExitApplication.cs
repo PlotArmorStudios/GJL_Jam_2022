02d99060e6ac0d67b2ae362df2666582e9b0f8d8
Protagonist <49970830+PlotArmorStudios@users.noreply.github.com>
2022-03-23 14:52:39 -0400

    made exit button work

--- GJL Jam 2022/Assets/Scripts/Game Management/ExitApplication.cs
+++ GJL Jam 2022/Assets/Scripts/Game Management/ExitApplication.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ExitApplication : MonoBehaviour
 {
     public void Quit()
     {
+        StartCoroutine(DelayQuit());
+    }
+
+    private IEnumerator DelayQuit()
+    {
+        var buttons = FindObjectsOfType<Button>();
+
+        foreach (var button in buttons)
+        {
+            if (button != this.GetComponent<Button>())
+                button.interactable = false;
+        }
+
+        yield return new WaitForSeconds(1f);
         Application.Quit();
     }
 }
