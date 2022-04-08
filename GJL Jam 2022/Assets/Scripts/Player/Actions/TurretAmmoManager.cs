9ad16d6298a805a28e9e9f2c28f438dd0f6ae76b
Khenan <49970830+PlotArmorStudios@users.noreply.github.com>
2022-04-08 17:17:34 -0400

    Update TurretAmmoManager.cs

--- GJL Jam 2022/Assets/Scripts/Player/Actions/TurretAmmoManager.cs
+++ GJL Jam 2022/Assets/Scripts/Player/Actions/TurretAmmoManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Intentionally left blank to house Turret ammo manager specific variables
+
 public class TurretAmmoManager : AmmoManager
 {
     public override void AddAmmo()
